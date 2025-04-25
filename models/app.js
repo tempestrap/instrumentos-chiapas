706c053158947e8ecd8fe38a83d56fc0c2fb1bc1
tempestrap <marquitosfer09@gmail.com>
2025-04-24 19:07:12 -0600

    cambios

--- models/app.js
+++ models/app.js
@@ -1,20 +1,20 @@
 import mysql from 'mysql2/promise';
 
 export const pool = mysql.createPool({
-  host: '172.31.47.107',  
-  user: 'mescobar',       
-  password: 'AAaa1100++', 
-  database: 'TALLER', 
-  port: 3306          
-});
-
-pool
-    .query('SELECT 1')
-    .then(() => console.log('Connected to MySQL'))
-    .catch((err) => {
-        console.error('Eroor al conectar a la base de datos', err)
-    })
-
-    pool.on('error', (err) => {
-        console.error('MySQL Pool Error', err)
-    })
+    host: '172.31.47.107',  
+    user: 'mescobar',       
+    password: 'AAaa1100++', 
+    database: 'TALLER', 
+    port: 3306          
+  });
+  
+  pool
+      .query('SELECT 1')
+      .then(() => console.log('Connected to MySQL'))
+      .catch((err) => {
+          console.error('Eroor al conectar a la base de datos', err)
+      })
+  
+      pool.on('error', (err) => {
+          console.error('MySQL Pool Error', err)
+      })
