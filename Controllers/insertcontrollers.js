feebc7e06a2e574c375bda60e17f2488217cd29f
tempestrap <marquitosfer09@gmail.com>
2025-04-26 14:59:56 -0600

    De momento las cosas estan en php pero vi un error, no conecten nada a php, de momento

--- Controllers/insertcontrollers.js
+++
@@ -1,7 +0,0 @@
-import {pool} from "../models/app.js";
-
-export const getINSERT = async(req,res) => {
-  const [rows] = await pool.query('INSERT INTO Usuario (nombre_usuario, contraseña) VALUES ("TEMPEST", "sefghfdghfh")')
-  //const [rows1] = await pool.query('VALUES ("TEMPEST", "hola1019")')
-  res.json(rows)
-}
--- Controllers/usuarioControllers.js
+++
@@ -1,6 +0,0 @@
-import {pool} from "../models/app.js";
-
-export const getUsuario = async(req,res) => {
-  const [rows] = await pool.query('SELECT * FROM Usuario')
-  res.json(rows)
-}
