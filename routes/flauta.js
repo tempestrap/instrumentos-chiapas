d5bc6ee3630878ec4de49c053298e8c1516b6d01
tempestrap <marquitosfer09@gmail.com>
2025-04-26 21:29:25 -0600

    ultimos instrumentos agregados, ayudenme nada ams con el front

---
+++ routes/flauta.js
@@ -0,0 +1,37 @@
+// marimba.js
+fetch('../db/instrumentos.json')
+  .then(response => response.json())
+  .then(data => {
+    const contenedor = document.getElementById('contenedor-instrumentos');
+    const marimbas = data.instrumentos.filter(instr => instr.categoria === 'flauta');
+
+    marimbas.forEach(instr => {
+      const html = `
+        <div class="instrumento">
+          <img src="../img/${instr.imagen}" alt="${instr.nombre}">
+          
+          <h1>${instr.nombre}</h1>
+
+          <div class="descripcion">
+            ${instr.descripcion}
+          </div>
+
+          <div class="caracteristicas">
+            ${instr.caracteristicas.map(caracteristica => `- ${caracteristica}<br>`).join('')}
+          </div>
+
+          <div class="marca">
+            Marca: ${instr.marca}
+          </div>
+
+          <div class="precio">
+            Precio: ${instr.precio}
+          </div>
+
+          <button class="comprar-btn">Comprar</button>
+        </div>
+      `;
+      contenedor.innerHTML += html;
+    });
+  })
+  .catch(error => console.error('Error cargando instrumentos:', error));
